c30f635476c4e0154d0cb67417ef73b65095d338
satishkashyap0423 <satishkashyap0423@gmail.com>
2020-04-23 13:25:35 +0530

    hello world

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
+
 const express = require('express')
-var mysql = require('mysql');
 const app = express()
 const port = 3000
-// var con = mysql.createConnection({
-//     host: "www.000webhost.com",
-//     user: "Satish",
-//     password: "791\p+W81S!AHsl}"
-//   });
-  var con = mysql.createConnection({
-    host : '',
-    user : "id13373715_satish",
-    password : "791\p+W81S!AHsl}",
-    database : 'id13373715_firstapp',
-    port:3306,
-    timeout: 10000000
-  });
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
